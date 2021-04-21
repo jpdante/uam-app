468756173a02ab4a1ed9c4378b54ba87569bd241
João Pedro Caires <jpdante@tryhosting.com.br>
2021-04-21 15:25:56 -0300

    Update bottom-bar

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -11,13 +11,7 @@ import { SettingsPageRoutingModule } from './settings-routing.module';
 import { SettingsPage } from './settings.page';
 
 @NgModule({
-  imports: [
-    BottomBarComponent,
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    SettingsPageRoutingModule
-  ],
-  declarations: [SettingsPage]
+  imports: [CommonModule, FormsModule, IonicModule, SettingsPageRoutingModule],
+  declarations: [SettingsPage, BottomBarComponent],
 })
 export class SettingsPageModule {}
