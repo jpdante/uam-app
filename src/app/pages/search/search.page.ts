9bd9166a327d5e505a1cfe12cb3ee9d3709389aa
Nícolas Ribeiro Marques <56075706+NicolasRMarques@users.noreply.github.com>
2021-04-23 15:25:57 -0300

    Interface de elementos

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 //import { TextInput } from 'ionic-angular';
 
+interface Elements {
+  name : string,
+  description: string
+}
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
@@ -16,4 +21,23 @@ export class SearchPage implements OnInit {
   /*onSearch(e: TextInput){
     console.error(e.TextInput);
   }*/
+
+  public elementsList : Elements[] = [
+    {
+      name : "Healing Potion" ,
+      description : "1d6" 
+    },
+    {
+      name : "Duck" ,
+      description : "1d100" 
+    },
+    {
+      name : "Long Sword" ,
+      description : "1d8" 
+    },
+    {
+      name : "Mother-in-Law" ,
+      description : "1dbillion" 
+    },
+  ];
 }
