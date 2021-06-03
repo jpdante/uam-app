d612e3c2aada5f268ce1f9a5316b59c4698e216a
Rafael Augusto De Souza Reis <lucario.jg@gmail.com>
2021-06-03 11:25:17 -0300

    Code Refatoration and finish Subsearch

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -13,236 +13,43 @@ interface Elements {
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
-  public type: string
-  public subtype : string
+  public type: string;
+  public subtype: string;
+  public verifyType = "classes"
+
 
   constructor() {}
 
   ngOnInit() {}
 
   onSearch(e: any) {
-    console.log(this.type)
-    switch (this.type) {
-      case "1":
-        Net.get('https://www.dnd5eapi.co/api/classes?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'class',
-                  name: element.name,
-                });
-              });
-            }
-          })
-          .catch((e: any) => {
-            console.error(e);
-          });
-          switch (this.subtype) {
-            case "1":
-              Net.get('https://www.dnd5eapi.co/api/classes?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'class',
-                  name: element.name,
-                });
-              });
-            }
-          })
-              break;
-            case "2":
-              Net.get('https://www.dnd5eapi.co/api/subclasses?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'subclass',
-                  name: element.name,
-                });
-              });
-            }
-          })
-              break;
-            case "3":
-              Net.get('https://www.dnd5eapi.co/api/features?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'feature',
-                  name: element.name,
-                });
-              });
-            }
-          })
-              break;
-            case "4":
-              Net.get('https://www.dnd5eapi.co/api/starting-equipment?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'Starting Equipmente',
-                  name: element.name,
-                });
-              });
-            }
-          })
-              break;
-          
-            default:
-              break;
-          }
-        break;
-      case "2":
-        Net.get('https://www.dnd5eapi.co/api/races?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'race',
-                  name: element.name,
-                });
-              });
-            }
-          })
-          .catch((e: any) => {
-            console.error(e);
-          });
-          switch (this.subtype) {
-            case "1":
-              Net.get('https://www.dnd5eapi.co/api/races?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'race',
-                  name: element.name,
-                });
-              });
-            }
-          })
-              break;
-            case "2":
-              Net.get('https://www.dnd5eapi.co/api/subraces?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'subrace',
-                  name: element.name,
-                });
-              });
-            }
-          })
-              break;
-            case "3":
-              Net.get('https://www.dnd5eapi.co/api/traits?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'trait',
-                  name: element.name,
-                });
-              });
-            }
-          })
-              break;
+    this.search(this.type, this.subtype, e.target.value);
+  }
 
-            default:
-              break;
-          }
-        break;
-      case "3":
-        Net.get('https://www.dnd5eapi.co/api/equipment?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'equipment',
-                  name: element.name,
-                });
-              });
-            }
-          })
-          .catch((e: any) => {
-            console.error(e);
-          });
-        break;
-      case "4":
-        Net.get('https://www.dnd5eapi.co/api/spells?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'spell',
-                  name: element.name,
-                });
-              });
-            }
-          })
-          .catch((e: any) => {
-            console.error(e);
-          });
-        break;
-      case "5":
-        Net.get('https://www.dnd5eapi.co/api/monsters?name=' + e.target.value)
-          .then((e: any) => {
-            console.log(e.data);
-            this.elementsList = [];
-            if (e.data) {
-              e.data.results.forEach((element) => {
-                this.elementsList.push({
-                  index: element.index,
-                  type: 'monster',
-                  name: element.name,
-                });
-              });
-            }
-          })
-          .catch((e: any) => {
-            console.error(e);
-          });
-        break;
-      default:
-        break;
+  public search(type: string, subtype: string | undefined, name: string) {
+    let url;
+    if (subtype === undefined) {
+      url = `https://www.dnd5eapi.co/api/${type}?name=${name}`
+    } else {
+      url = `https://www.dnd5eapi.co/api/${subtype}?name=${name}`
     }
+    Net.get(url)
+        .then((e: any) => {
+          console.log(e.data);
+          this.elementsList = [];
+          if (e.data) {
+            e.data.results.forEach((element) => {
+              this.elementsList.push({
+                index: element.index,
+                type: type,
+                name: element.name,
+              });
+            });
+          }
+        })
+        .catch((e: any) => {
+          console.error(e);
+        });
   }
 
   public elementsList: Elements[] = [];
