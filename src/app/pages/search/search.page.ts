44022c569ae938bc066337a9fdfc52ae0a41d71d
Rafael Augusto De Souza Reis <lucario.jg@gmail.com>
2021-06-02 22:37:55 -0300

    Finish subsearch almost

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -13,7 +13,8 @@ interface Elements {
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
-  public type: string;
+  public type: string
+  public subtype : string
 
   constructor() {}
 
@@ -40,6 +41,75 @@ export class SearchPage implements OnInit {
           .catch((e: any) => {
             console.error(e);
           });
+          switch (this.subtype) {
+            case "1":
+              Net.get('https://www.dnd5eapi.co/api/classes?name=' + e.target.value)
+          .then((e: any) => {
+            console.log(e.data);
+            this.elementsList = [];
+            if (e.data) {
+              e.data.results.forEach((element) => {
+                this.elementsList.push({
+                  index: element.index,
+                  type: 'class',
+                  name: element.name,
+                });
+              });
+            }
+          })
+              break;
+            case "2":
+              Net.get('https://www.dnd5eapi.co/api/subclasses?name=' + e.target.value)
+          .then((e: any) => {
+            console.log(e.data);
+            this.elementsList = [];
+            if (e.data) {
+              e.data.results.forEach((element) => {
+                this.elementsList.push({
+                  index: element.index,
+                  type: 'subclass',
+                  name: element.name,
+                });
+              });
+            }
+          })
+              break;
+            case "3":
+              Net.get('https://www.dnd5eapi.co/api/features?name=' + e.target.value)
+          .then((e: any) => {
+            console.log(e.data);
+            this.elementsList = [];
+            if (e.data) {
+              e.data.results.forEach((element) => {
+                this.elementsList.push({
+                  index: element.index,
+                  type: 'feature',
+                  name: element.name,
+                });
+              });
+            }
+          })
+              break;
+            case "4":
+              Net.get('https://www.dnd5eapi.co/api/starting-equipment?name=' + e.target.value)
+          .then((e: any) => {
+            console.log(e.data);
+            this.elementsList = [];
+            if (e.data) {
+              e.data.results.forEach((element) => {
+                this.elementsList.push({
+                  index: element.index,
+                  type: 'Starting Equipmente',
+                  name: element.name,
+                });
+              });
+            }
+          })
+              break;
+          
+            default:
+              break;
+          }
         break;
       case "2":
         Net.get('https://www.dnd5eapi.co/api/races?name=' + e.target.value)
@@ -59,6 +129,59 @@ export class SearchPage implements OnInit {
           .catch((e: any) => {
             console.error(e);
           });
+          switch (this.subtype) {
+            case "1":
+              Net.get('https://www.dnd5eapi.co/api/races?name=' + e.target.value)
+          .then((e: any) => {
+            console.log(e.data);
+            this.elementsList = [];
+            if (e.data) {
+              e.data.results.forEach((element) => {
+                this.elementsList.push({
+                  index: element.index,
+                  type: 'race',
+                  name: element.name,
+                });
+              });
+            }
+          })
+              break;
+            case "2":
+              Net.get('https://www.dnd5eapi.co/api/subraces?name=' + e.target.value)
+          .then((e: any) => {
+            console.log(e.data);
+            this.elementsList = [];
+            if (e.data) {
+              e.data.results.forEach((element) => {
+                this.elementsList.push({
+                  index: element.index,
+                  type: 'subrace',
+                  name: element.name,
+                });
+              });
+            }
+          })
+              break;
+            case "3":
+              Net.get('https://www.dnd5eapi.co/api/traits?name=' + e.target.value)
+          .then((e: any) => {
+            console.log(e.data);
+            this.elementsList = [];
+            if (e.data) {
+              e.data.results.forEach((element) => {
+                this.elementsList.push({
+                  index: element.index,
+                  type: 'trait',
+                  name: element.name,
+                });
+              });
+            }
+          })
+              break;
+
+            default:
+              break;
+          }
         break;
       case "3":
         Net.get('https://www.dnd5eapi.co/api/equipment?name=' + e.target.value)
@@ -80,7 +203,6 @@ export class SearchPage implements OnInit {
           });
         break;
       case "4":
-        console.log("ola amigo")
         Net.get('https://www.dnd5eapi.co/api/spells?name=' + e.target.value)
           .then((e: any) => {
             console.log(e.data);
