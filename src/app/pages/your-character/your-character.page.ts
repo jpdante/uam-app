158714923aa6c24fb50466d4ebb8580d10fd90ec
Nícolas Ribeiro Marques <56075706+NicolasRMarques@users.noreply.github.com>
2021-06-06 17:30:37 -0300

    Simplificando o save

--- src/app/pages/your-character/your-character.page.ts
+++ src/app/pages/your-character/your-character.page.ts
@@ -75,170 +75,137 @@ export class YourCharacterPage implements OnInit {
   public async changeLifePoints (life: number){
     if(life < 1000 && life > 0)
     this.character.lifePoints = life;
-    this.getSetSave();
   }
 
   public async changeArmorClass (armor: number){
     if(armor < 100 && armor > 0)
     this.character.armorClass = armor; 
-    this.getSetSave();
   }
 
   public async levelUp (){
     if(this.character.level < 20)
     this.character.level += 1; 
-    this.getSetSave();
   }
   
   public async levelDown (){
     if(this.character.level > 1)
     this.character.level -= 1;
-    this.getSetSave();
   }
 
   public async strengthUp (){
     if(this.character.atrib.strength < 30)
     this.character.atrib.strength += 1; 
-    this.getSetSave();
   }
   
   public async strengthDown (){
     if(this.character.atrib.strength > 1)
     this.character.atrib.strength -= 1;
-    this.getSetSave();
+
   }
 
   public async dexterityUp (){
     if(this.character.atrib.dexterity < 30)
     this.character.atrib.dexterity += 1; 
-    this.getSetSave();
   }
   
   public async dexterityDown (){
     if(this.character.atrib.dexterity > 1)
     this.character.atrib.dexterity -= 1;
-    this.getSetSave();
   }
 
   public async constitutionUp (){
     if(this.character.atrib.constitution < 30)
     this.character.atrib.constitution += 1; 
-    this.getSetSave();
   }
   
   public async constitutionDown (){
     if(this.character.atrib.constitution > 1)
     this.character.atrib.constitution -= 1;
-    this.getSetSave();
   }
 
   public async intelligenceUp (){
     if(this.character.atrib.intelligence < 30)
     this.character.atrib.intelligence += 1; 
-    this.getSetSave();
   }
   
   public async intelligenceDown (){
     if(this.character.atrib.intelligence > 1)
     this.character.atrib.intelligence -= 1;
-    this.getSetSave();
   }
 
   public async wisdomUp (){
     if(this.character.atrib.wisdom < 30)
     this.character.atrib.wisdom += 1; 
-    this.getSetSave();
   }
   
   public async wisdomDown (){
     if(this.character.atrib.wisdom > 1)
     this.character.atrib.wisdom -= 1;
-    this.getSetSave();
   }
 
   public async charismaUp (){
     if(this.character.atrib.charisma < 30)
     this.character.atrib.charisma += 1; 
-    this.getSetSave();
   }
   
   public async charismaDown (){
     if(this.character.atrib.charisma > 1)
     this.character.atrib.charisma -= 1;
-    this.getSetSave();
   }
 
   public async changeCharacterStory (text: string){
     this.character.story = text; 
-    this.getSetSave();
   }
 
   public async changeItens (text: string){
     this.character.itensList = text;
-    this.getSetSave();
   }
 
   public async changeSpells (text: string){
     this.character.spellsList = text;
-    this.getSetSave();
   }
 
   public async changeLanguagesAndProeficiencies (text: string){
     this.character.languagesAndProficiencies = text;
-    this.getSetSave();
   }
 
   public async changeXp (number: number){
     if(number < 1000000000 && number > 0)
     this.character.xp = number;
-    this.getSetSave();
   }
 
   public async changeAlignment  (text: string){
     this.character.alignment = text;
-    this.getSetSave();
   }
 
   public async changeInitiative (number: number){
     if(number < 21 && number > 0)
     this.character.initiative = number;
-    this.getSetSave();
   }
 
   public async changeSpeed (number: number){
     if(number < 100 && number > 0)
     this.character.speed = number;
-    this.getSetSave();
   }
 
   public async changeName (string: string){
     if(string != '') 
     this.character.name = string;
-    this.getSetSave();
   }
 
   public async changeGender (string: string){
     if(string != '') 
     this.character.gender = string;
-    this.getSetSave();
   }
   
   public async changeRace (string: string){
     if(string != '') 
     this.character.race = string;
-    this.getSetSave();
   }
 
   public async changeClass (string: string){
     if(string != '') 
     this.character.class = string;
-    this.getSetSave();
-  }
-
-  public async getSetSave (){
-    let characters = await this.storageService.get ('characters');
-    characters[this.id] = this.character;
-    await this.storageService.set('characters', characters);
   }
 
   public async savePage (a: number, b: number, c: string, d: string, e: string, f: string, g: number, h: string, i: number, j: number, k: string,  l: string,  m: string,  n: string,){
@@ -256,6 +223,9 @@ export class YourCharacterPage implements OnInit {
     this.changeGender(l);
     this.changeRace(m);
     this.changeClass(n);
+    let characters = await this.storageService.get ('characters');
+    characters[this.id] = this.character;
+    await this.storageService.set('characters', characters);
   }
 
   public calculateMod (ability: number){
